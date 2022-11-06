c120d75122ba1f91daec3a3db93852b2603437ad
Divyansh Gupta <divyanshguptarocking@gmail.com>
2022-11-06 16:40:32 +0800

    chore: add dummy torrent file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('Hello world!')
+import { join } from 'path'
+import { readFileSync } from 'fs'
+
+const FILE_NAME = 'puppy.torrent'
+
+function parseTorrent(): void {
+  const filePath = join(process.cwd(), FILE_NAME)
+  const torrent = readFileSync(filePath).toString('utf-8')
+  console.log(torrent)
+}
+
+parseTorrent()
