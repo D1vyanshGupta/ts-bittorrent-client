9029e24348fd23a6deefade6812a7b79b4fc044b
Divyansh Gupta <divyanshguptarocking@gmail.com>
2022-11-12 11:50:17 +0800

    refactor: logTorrent

--- src/helpers/logging.ts
+++ src/helpers/logging.ts
@@ -1,4 +1,23 @@
 import pino from 'pino'
 import pretty from 'pino-pretty'
 
-export const logger = pino(pretty())
+const logger = pino(pretty())
+
+function isObject(value: unknown): boolean {
+  return typeof value === 'object' && !Array.isArray(value) && value !== null
+}
+
+export function logTorrent(object: object, parentKey = ''): void {
+  //eslint-disable-next-line no-loops/no-loops
+  for (const [key, value] of Object.entries(object)) {
+    const printKey = parentKey + key
+
+    if (Buffer.isBuffer(value)) {
+      const encoding = key === 'pieces' ? 'hex' : 'utf8'
+      logger.info(`${printKey} => ${value.toString(encoding)}`)
+    } else if (isObject(value)) {
+      logger.info(`${printKey}`)
+      logTorrent(value, parentKey + '\t')
+    } else logger.info(`${printKey} => ${value}`)
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,28 +2,9 @@ import { join } from 'path'
 import { decode } from 'bencode'
 import { readFileSync } from 'fs'
 
-import { logger } from './helpers/logging'
+import { logTorrent } from './helpers/logging'
 
 const FILE_NAME = 'puppy.torrent'
-
-function isObject(value: unknown): boolean {
-  return typeof value === 'object' && !Array.isArray(value) && value !== null
-}
-
-function logTorrent(object: object, parentKey = ''): void {
-  //eslint-disable-next-line no-loops/no-loops
-  for (const [key, value] of Object.entries(object)) {
-    const printKey = parentKey + key
-
-    if (Buffer.isBuffer(value)) {
-      const encoding = key === 'pieces' ? 'hex' : 'utf8'
-      logger.info(`${printKey} => ${value.toString(encoding)}`)
-    } else if (isObject(value)) {
-      logger.info(`${printKey}`)
-      logTorrent(value, parentKey + '\t')
-    } else logger.info(`${printKey} => ${value}`)
-  }
-}
 
 function parseTorrent(): void {
   const filePath = join(process.cwd(), FILE_NAME)
